ea3a936a193c3deb83bb3506be86ad02d5aacf88
fadialjohari0 <fadialjohari0@gmail.com>
2024-06-26 04:02:24 +0300

    Add validator regex for URL

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,5 +1,10 @@
 const isEmail = email => email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+const isURL = url =>
+  url.match(
+    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+  );
 
 export const ValidationHelper = {
   isEmail,
+  isURL,
 };
